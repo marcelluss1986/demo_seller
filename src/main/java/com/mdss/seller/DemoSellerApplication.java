4f4f0887b02ec73813bdf8e9d47e222523d6d302
Marcelo Soares <marcelossoares@live.com>
2022-06-11 04:39:14 -0300

    Created Project

---
+++ src/main/java/com/mdss/seller/DemoSellerApplication.java
@@ -0,0 +1,13 @@
+package com.mdss.seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdss/seller/DemoSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdss.seller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
