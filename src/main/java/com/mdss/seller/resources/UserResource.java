ca39ed39d543def51c6556d79233f74ccb509c95
Marcelo Soares <marcelossoares@live.com>
2022-06-14 06:47:27 -0300

    Update and Delete method

--- src/main/java/com/mdss/seller/resources/UserResource.java
+++ src/main/java/com/mdss/seller/resources/UserResource.java
@@ -1,12 +1,16 @@
 package com.mdss.seller.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mdss.seller.entities.User;
 import com.mdss.seller.services.UserService;
@@ -22,5 +26,24 @@ public class UserResource {
 		public ResponseEntity<List<User>> findAll(){
 			List<User>obj = services.findAll();
 			return ResponseEntity.ok().body(obj);
+		}
+		
+		@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+		public ResponseEntity<User> findById(@PathVariable Long id){
+			User obj = services.findById(id);
+			return ResponseEntity.ok().body(obj);
+		}
+		
+		@RequestMapping(method = RequestMethod.POST)
+		public ResponseEntity<User> insert(@RequestBody User obj){
+			User user = services.insert(obj);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+			return ResponseEntity.created(uri).body(obj);
+		}
+		
+		@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+		public ResponseEntity<Void> delete(@PathVariable Long id){
+			services.delete(id);
+			return ResponseEntity.noContent().build();
 		}
 }
--- src/main/java/com/mdss/seller/services/UserService.java
+++ src/main/java/com/mdss/seller/services/UserService.java
@@ -1,6 +1,7 @@
 package com.mdss.seller.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,31 @@ public class UserService {
 		public List<User> findAll(){
 			List<User> obj = repository.findAll();
 			return obj;
+		}
+		
+		public User findById(Long id) {
+			Optional<User> obj = repository.findById(id);
+			return obj.get();
+		}
+		
+		public User insert (User obj) {
+			return repository.save(obj);
+		}
+		
+		public void delete(Long id) {
+			repository.deleteById(id);
+		}
+		
+		public User update(Long id, User obj) {
+			Optional<User> entity = repository.findById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+			
+		}
+
+		private void updateData(Optional<User> entity, User obj) {
+			entity.get().setName(obj.getName());
+			entity.get().setEmail(obj.getEmail());
+			entity.get().setPhone(obj.getPhone());
 		}
 }
